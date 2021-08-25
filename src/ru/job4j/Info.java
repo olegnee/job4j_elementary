29d8e664b6dcdc2a2d9cca94625b1ffb6e5db4c0
olegnee <oleg.nee92@gmail.com>
2021-08-25 23:02:22 +0900

    3. Арифметические операции. [#185]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,8 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("2021.08.24");
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
     }
 }
